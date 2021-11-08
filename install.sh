65ecdb75a7794403bb9e286956521bb02ee3074e
Oleksandr Nosov <acnosov@gmail.com>
2021-11-08 19:28:56 +0200

    add firewall

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 echo "install htop --needed"
 sudo pacman -Syu
 sudo pacman -S htop --needed 
+sudo pacman -S nvtop --needed 
 pamac install ufw
+pamac install ufw-extras
+pamac install samba kdenetwork-filesharing manjaro-settings-samba
+sudo ufw allow CIFS
+
 echo "world"
 
-pamac install ttf-ms-fonts
-pamac install nerd-fonts-complete  
+# pamac install ttf-ms-fonts
+# pamac install nerd-fonts-complete  
