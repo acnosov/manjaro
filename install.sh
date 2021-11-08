a32b5ff83976752c1332d6f6dc44b1bc2910850b
Oleksandr Nosov <acnosov@gmail.com>
2021-11-08 16:54:21 +0200

    add fonts

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-echo "hello"
+echo "install htop --needed"
+sudo pacman -Syu
+sudo pacman -S htop --needed 
+pamac install ufw
+echo "world"
+
+pamac install ttf-ms-fonts
+pamac install nerd-fonts-complete  
